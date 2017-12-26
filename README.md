5ad5f47b9319d3d081f719515351c3944a24f928
centos7 <centos7@localhost>
2017-12-26 09:19:14 +0800

    no-java

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Introduction
-A distribution of Nginx with c++,python,lua java and php web development. 
+A distribution of Nginx with c++,python,lua and php web development. 
 
 
 [hi-nginx-demo](https://github.com/webcpp/hi_demo).
@@ -68,33 +68,6 @@ install hello.so /usr/local/nginx/hi
 
 ```
 
-## java servlet class
-
-```
-package hi;
-
-public class jhello implements hi.servlet {
-
-    public jhello() {
-
-    }
-
-    public void handler(hi.request req, hi.response res) {
-        res.status = 200;
-        res.content = "hello,world";
-
-    }
-}
-
-```
-
-### java compile
-
-```
-${JAVA_HOME}/bin/javac -classpath .:${NGINX_INSTALL_DIR}/java/hi-nginx-java.jar hi/jhello.java
-
-```
-
 
 ## php servlet class
 
@@ -128,7 +101,6 @@ see `contrbi/php`
 - python-devel
 - boost-devel
 - luajit-devel
-- jdk 1.1,1.2,1.4,1.6,1.8,9
 - PHP 7.0 or later(--enable-embed=shared)
 
 ## centos
@@ -140,25 +112,6 @@ sudo yum install gcc gcc-c++ make pcre-devel zlib-devel openssl-devel hiredis-de
 ```
 sudo apt-get install build-essential libpcre3-dev zlib1g-dev libssl-dev libhiredis-dev python-dev libboost-all-dev libluajit-5.1-dev 
 
-```
-
-## About jdk 9 install
-```
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk selfupdate force
-sdk install java 9.0.1-oracle
-
-```
-
-Create the file /etc/ld.so.conf.d/java.conf with the following entries，replace $JAVA_HOME as true path:
-```
-$JAVA_HOME/lib/
-$JAVA_HOME/lib/server
-```
-then
-```
-sudo ldconfig
 ```
 
 ## About php7+ install
@@ -336,57 +289,7 @@ see `install_demo.sh` or `--add-module=ngx_http_hi_module`
                 hi_lua_script lua;
             }
 ```
-- directives : content: http,srv,if in srv
-    - hi_java_classpath,default:"-Djava.class.path=."
 
-    example:
-```
-hi_java_classpath "-Djava.class.path=.:/usr/local/nginx/java:/usr/local/nginx/java/hi-nginx-java.jar";
-
-```
-
-- directives : content: http,srv,if in srv
-    - hi_java_options,default:"-server -d64 -Xmx1G -Xms1G -Xmn256m"
-
-    example:
-```
-hi_java_options""-server -d64 -Xmx3G -Xms3G -Xmn768m -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+UseNUMA -XX:+CMSParallelRemarkEnabled -XX:MaxTenuringThreshold=15 -XX:MaxGCPauseMillis=30 -XX:GCPauseIntervalMillis=150 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -Dfml.ignorePatchDiscrepancies=true -Dfml.ignoreInvalidMinecraftCertificates=true -XX:+UseFastAccessorMethods -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:+AggressiveOpts -XX:ReservedCodeCacheSize=2048m -XX:+UseCodeCacheFlushing -XX:SoftRefLRUPolicyMSPerMB=10000 -XX:ParallelGCThreads=10"";
-
-```
-
-- directives : content: loc,if in loc
-    - hi_java_servlet,default:""
-
-    example:
-```
-hi_java_servlet hi/jhello;
-
-
-```
-- directives : content : http,srv,loc,if in loc ,if in srv
-    - hi_java_servlet_cache_expires,default:300s
-
-    example:
-```
-hi_java_servlet_cache_expires 300s;
-
-```
-- directives : content : http,srv,loc,if in loc ,if in srv
-    - hi_java_servlet_cache_size,default:10
-
-    example:
-```
-hi_java_servlet_cache_size 10;
-
-```
-- directives : content: http,srv,if in srv
-    - hi_java_version,default:9
-
-    example:
-```
-hi_java_version 9;
-
-```
 - directives : content: loc,if in loc
     - hi_php_script,default: ""
 
--- ngx_http_hi_module/ngx_http_hi_module.cpp
+++ ngx_http_hi_module/ngx_http_hi_module.cpp
@@ -39,7 +39,7 @@ extern "C" {
 
 #include "lib/lua.hpp"
 
-#include "lib/java.hpp"
+
 
 
 #include "lib/MPFDParser-1.1.1/Parser.h"
@@ -62,13 +62,10 @@ static std::vector<std::shared_ptr<hi::cache::lru_cache<std::string, cache_ele_t
 static std::shared_ptr<hi::redis> REDIS;
 static std::shared_ptr<hi::boost_py> PYTHON;
 static std::shared_ptr<hi::lua> LUA;
-static std::shared_ptr<hi::java> JAVA;
-static std::shared_ptr<hi::cache::lru_cache<std::string, hi::java_servlet_t>> JAVA_SERVLET_CACHE;
-static bool JAVA_IS_READY = false;
 static std::shared_ptr<php::VM> PHP;
 
 enum application_t {
-    __cpp__, __python__, __lua__, __java__, __php__, __unkown__
+    __cpp__, __python__, __lua__, __php__, __unkown__
 };
 
 typedef struct {
@@ -78,19 +75,13 @@ typedef struct {
     , python_content
     , lua_script
     , lua_content
-    , java_classpath
-    , java_options
-    , java_servlet
     , php_script;
     ngx_int_t redis_port
     , module_index
     , cache_expires
     , session_expires
-    , cache_index
-    , java_servlet_cache_expires
-    , java_version;
-    size_t cache_size
-    , java_servlet_cache_size;
+    , cache_index;
+    size_t cache_size;
     ngx_flag_t need_headers
     , need_cache
     , need_cookies
@@ -117,11 +108,6 @@ static ngx_str_t get_input_body(ngx_http_request_t *r);
 static void ngx_http_hi_cpp_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res);
 static void ngx_http_hi_python_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res);
 static void ngx_http_hi_lua_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res);
-static void ngx_http_hi_java_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res);
-
-static void java_input_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res, jobject request_instance, jobject response_instance);
-static void java_output_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res, jobject request_instance, jobject response_instance);
-static bool java_init_handler(ngx_http_hi_loc_conf_t * conf);
 
 static void ngx_http_hi_php_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res);
 
@@ -243,54 +229,6 @@ ngx_command_t ngx_http_hi_commands[] = {
         NULL
     },
     {
-        ngx_string("hi_java_classpath"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_SIF_CONF | NGX_CONF_TAKE1,
-        ngx_conf_set_str_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_classpath),
-        NULL
-    },
-    {
-        ngx_string("hi_java_options"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_SIF_CONF | NGX_CONF_TAKE1,
-        ngx_conf_set_str_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_options),
-        NULL
-    },
-    {
-        ngx_string("hi_java_servlet"),
-        NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
-        ngx_http_hi_conf_init,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_servlet),
-        NULL
-    },
-    {
-        ngx_string("hi_java_servlet_cache_expires"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_SIF_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
-        ngx_conf_set_sec_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_servlet_cache_expires),
-        NULL
-    },
-    {
-        ngx_string("hi_java_servlet_cache_size"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_SIF_CONF | NGX_CONF_TAKE1,
-        ngx_conf_set_num_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_servlet_cache_size),
-        NULL
-    },
-    {
-        ngx_string("hi_java_version"),
-        NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_SIF_CONF | NGX_CONF_TAKE1,
-        ngx_conf_set_num_slot,
-        NGX_HTTP_LOC_CONF_OFFSET,
-        offsetof(ngx_http_hi_loc_conf_t, java_version),
-        NULL
-    },
-    {
         ngx_string("hi_php_script"),
         NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
         ngx_http_hi_conf_init,
@@ -339,9 +277,6 @@ static ngx_int_t clean_up(ngx_conf_t *cf) {
     REDIS.reset();
     PYTHON.reset();
     LUA.reset();
-    JAVA.reset();
-    JAVA_SERVLET_CACHE.reset();
-    JAVA_IS_READY = false;
     PHP.reset();
     return NGX_OK;
 }
@@ -372,13 +307,6 @@ static void * ngx_http_hi_create_loc_conf(ngx_conf_t *cf) {
         conf->lua_content.data = NULL;
         conf->php_script.len = 0;
         conf->php_script.data = NULL;
-        conf->java_classpath.len = 0;
-        conf->java_classpath.data = NULL;
-        conf->java_servlet.len = 0;
-        conf->java_servlet.data = NULL;
-        conf->java_servlet_cache_size = NGX_CONF_UNSET_UINT;
-        conf->java_servlet_cache_expires = NGX_CONF_UNSET;
-        conf->java_version = NGX_CONF_UNSET;
         conf->redis_port = NGX_CONF_UNSET;
         conf->cache_size = NGX_CONF_UNSET_UINT;
         conf->cache_expires = NGX_CONF_UNSET;
@@ -405,12 +333,6 @@ static char * ngx_http_hi_merge_loc_conf(ngx_conf_t* cf, void* parent, void* chi
     ngx_conf_merge_str_value(conf->lua_script, prev->lua_script, "");
     ngx_conf_merge_str_value(conf->lua_content, prev->lua_content, "");
     ngx_conf_merge_str_value(conf->php_script, prev->php_script, "");
-    ngx_conf_merge_str_value(conf->java_classpath, prev->java_classpath, "-Djava.class.path=.");
-    ngx_conf_merge_str_value(conf->java_options, prev->java_options, "-server -d64 -Xmx1G -Xms1G -Xmn256m");
-    ngx_conf_merge_str_value(conf->java_servlet, prev->java_servlet, "");
-    ngx_conf_merge_uint_value(conf->java_servlet_cache_size, prev->java_servlet_cache_size, (size_t) 10);
-    ngx_conf_merge_sec_value(conf->java_servlet_cache_expires, prev->java_servlet_cache_expires, (ngx_int_t) 300);
-    ngx_conf_merge_value(conf->java_version, prev->java_version, (ngx_int_t) 8);
     ngx_conf_merge_value(conf->redis_port, prev->redis_port, (ngx_int_t) 0);
     ngx_conf_merge_uint_value(conf->cache_size, prev->cache_size, (size_t) 10);
     ngx_conf_merge_sec_value(conf->cache_expires, prev->cache_expires, (ngx_int_t) 300);
@@ -454,12 +376,6 @@ static char * ngx_http_hi_merge_loc_conf(ngx_conf_t* cf, void* parent, void* chi
             int argc = 1;
             char* argv[2] = {"", NULL};
             PHP = std::move(std::make_shared<php::VM>(argc, argv));
-        }
-    }
-    if (conf->java_servlet.len > 0) {
-        conf->app_type = application_t::__java__;
-        if (!JAVA_SERVLET_CACHE) {
-            JAVA_SERVLET_CACHE = std::make_shared<hi::cache::lru_cache < std::string, hi::java_servlet_t >> (conf->java_servlet_cache_size);
         }
     }
 
@@ -629,8 +545,6 @@ static ngx_int_t ngx_http_hi_normal_handler(ngx_http_request_t *r) {
         case application_t::__python__:ngx_http_hi_python_handler(conf, ngx_request, ngx_response);
             break;
         case application_t::__lua__:ngx_http_hi_lua_handler(conf, ngx_request, ngx_response);
-            break;
-        case application_t::__java__:ngx_http_hi_java_handler(conf, ngx_request, ngx_response);
             break;
         case application_t::__php__:ngx_http_hi_php_handler(conf, ngx_request, ngx_response);
             break;
@@ -814,236 +728,6 @@ static void ngx_http_hi_lua_handler(ngx_http_hi_loc_conf_t * conf, hi::request& 
             LUA->call_content((char*) conf->lua_content.data);
         }
     }
-}
-
-static void ngx_http_hi_java_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res) {
-    if (java_init_handler(conf)) {
-
-        jobject request_instance, response_instance;
-
-
-        request_instance = JAVA->env->NewObject(JAVA->request, JAVA->request_ctor);
-        response_instance = JAVA->env->NewObject(JAVA->response, JAVA->response_ctor);
-
-        java_input_handler(conf, req, res, request_instance, response_instance);
-
-
-
-        hi::java_servlet_t jtmp;
-        if (JAVA_SERVLET_CACHE->exists((const char*) conf->java_servlet.data)) {
-            jtmp = JAVA_SERVLET_CACHE->get((const char*) conf->java_servlet.data);
-            time_t now = time(0);
-            if (difftime(now, jtmp.t) > conf->java_servlet_cache_expires) {
-                JAVA_SERVLET_CACHE->erase((const char*) conf->java_servlet.data);
-                goto java_servlet_update;
-            }
-        } else {
-java_servlet_update:
-            jtmp.SERVLET = JAVA->env->FindClass((const char*) conf->java_servlet.data);
-            if (jtmp.SERVLET == NULL)return;
-            jtmp.CTOR = JAVA->env->GetMethodID(jtmp.SERVLET, "<init>", "()V");
-            jtmp.HANDLER = JAVA->env->GetMethodID(jtmp.SERVLET, "handler", "(Lhi/request;Lhi/response;)V");
-            jtmp.t = time(0);
-            JAVA_SERVLET_CACHE->put((const char*) conf->java_servlet.data, jtmp);
-        }
-        jobject servlet_instance = JAVA->env->NewObject(jtmp.SERVLET, jtmp.CTOR);
-        JAVA->env->CallVoidMethod(servlet_instance, jtmp.HANDLER, request_instance, response_instance);
-        JAVA->env->DeleteLocalRef(servlet_instance);
-
-
-        java_output_handler(conf, req, res, request_instance, response_instance);
-
-
-        JAVA->env->DeleteLocalRef(request_instance);
-        JAVA->env->DeleteLocalRef(response_instance);
-    }
-
-}
-
-static void java_input_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res, jobject request_instance, jobject response_instance) {
-    jstring client = JAVA->env->NewStringUTF(req.client.c_str());
-    JAVA->env->SetObjectField(request_instance, JAVA->client, client);
-    JAVA->env->ReleaseStringUTFChars(client, 0);
-    JAVA->env->DeleteLocalRef(client);
-
-    jstring user_agent = JAVA->env->NewStringUTF(req.user_agent.c_str());
-    JAVA->env->SetObjectField(request_instance, JAVA->user_agent, user_agent);
-    JAVA->env->ReleaseStringUTFChars(user_agent, 0);
-    JAVA->env->DeleteLocalRef(user_agent);
-
-    jstring method = JAVA->env->NewStringUTF(req.method.c_str());
-    JAVA->env->SetObjectField(request_instance, JAVA->method, method);
-    JAVA->env->ReleaseStringUTFChars(method, 0);
-    JAVA->env->DeleteLocalRef(method);
-
-    jstring uri = JAVA->env->NewStringUTF(req.uri.c_str());
-    JAVA->env->SetObjectField(request_instance, JAVA->uri, uri);
-    JAVA->env->ReleaseStringUTFChars(uri, 0);
-    JAVA->env->DeleteLocalRef(uri);
-
-    jstring param = JAVA->env->NewStringUTF(req.param.c_str());
-    JAVA->env->SetObjectField(request_instance, JAVA->param, param);
-    JAVA->env->ReleaseStringUTFChars(param, 0);
-    JAVA->env->DeleteLocalRef(param);
-
-    if (conf->need_headers == 1) {
-        jobject req_headers = JAVA->env->GetObjectField(request_instance, JAVA->req_headers);
-        for (auto& item : req.headers) {
-            jstring k = JAVA->env->NewStringUTF(item.first.c_str())
-                    , v = JAVA->env->NewStringUTF(item.second.c_str());
-            JAVA->env->CallObjectMethod(req_headers, JAVA->hashmap_put, k, v);
-            JAVA->env->ReleaseStringUTFChars(k, 0);
-            JAVA->env->ReleaseStringUTFChars(v, 0);
-            JAVA->env->DeleteLocalRef(k);
-            JAVA->env->DeleteLocalRef(v);
-        }
-        JAVA->env->DeleteLocalRef(req_headers);
-    }
-
-    jobject req_form = JAVA->env->GetObjectField(request_instance, JAVA->form);
-    for (auto& item : req.form) {
-        jstring k = JAVA->env->NewStringUTF(item.first.c_str())
-                , v = JAVA->env->NewStringUTF(item.second.c_str());
-        JAVA->env->CallObjectMethod(req_form, JAVA->hashmap_put, k, v);
-        JAVA->env->ReleaseStringUTFChars(k, 0);
-        JAVA->env->ReleaseStringUTFChars(v, 0);
-        JAVA->env->DeleteLocalRef(k);
-        JAVA->env->DeleteLocalRef(v);
-    }
-    JAVA->env->DeleteLocalRef(req_form);
-
-    if (conf->need_cookies == 1) {
-        jobject req_cookies = JAVA->env->GetObjectField(request_instance, JAVA->cookies);
-        for (auto& item : req.cookies) {
-            jstring k = JAVA->env->NewStringUTF(item.first.c_str())
-                    , v = JAVA->env->NewStringUTF(item.second.c_str());
-            JAVA->env->CallObjectMethod(req_cookies, JAVA->hashmap_put, k, v);
-            JAVA->env->ReleaseStringUTFChars(k, 0);
-            JAVA->env->ReleaseStringUTFChars(v, 0);
-            JAVA->env->DeleteLocalRef(k);
-            JAVA->env->DeleteLocalRef(v);
-        }
-        JAVA->env->DeleteLocalRef(req_cookies);
-    }
-
-    if (conf->need_session == 1) {
-        jobject req_session = JAVA->env->GetObjectField(request_instance, JAVA->req_session);
-        for (auto& item : req.session) {
-            jstring k = JAVA->env->NewStringUTF(item.first.c_str())
-                    , v = JAVA->env->NewStringUTF(item.second.c_str());
-            JAVA->env->CallObjectMethod(req_session, JAVA->hashmap_put, k, v);
-            JAVA->env->ReleaseStringUTFChars(k, 0);
-            JAVA->env->ReleaseStringUTFChars(v, 0);
-            JAVA->env->DeleteLocalRef(k);
-            JAVA->env->DeleteLocalRef(v);
-        }
-        JAVA->env->DeleteLocalRef(req_session);
-    }
-
-}
-
-static void java_output_handler(ngx_http_hi_loc_conf_t * conf, hi::request& req, hi::response& res, jobject request_instance, jobject response_instance) {
-    jobject res_headers = JAVA->env->GetObjectField(response_instance, JAVA->res_headers);
-    jobject keyset = JAVA->env->CallObjectMethod(res_headers, JAVA->hashmap_keyset);
-    jobject iterator = JAVA->env->CallObjectMethod(keyset, JAVA->set_iterator);
-    while ((bool)JAVA->env->CallBooleanMethod(iterator, JAVA->hasnext)) {
-        jstring k = (jstring) JAVA->env->CallObjectMethod(iterator, JAVA->next);
-        jobject item = JAVA->env->CallObjectMethod(res_headers, JAVA->hashmap_get, k);
-        jobject jterator = JAVA->env->CallObjectMethod(item, JAVA->arraylist_iterator);
-        const char * kstr = JAVA->env->GetStringUTFChars(k, NULL);
-        while ((bool)JAVA->env->CallBooleanMethod(jterator, JAVA->hasnext)) {
-            jstring v = (jstring) JAVA->env->CallObjectMethod(jterator, JAVA->next);
-            const char* vstr = JAVA->env->GetStringUTFChars(v, NULL);
-            res.headers.insert(std::make_pair(kstr, vstr));
-            JAVA->env->ReleaseStringUTFChars(v, vstr);
-            JAVA->env->DeleteLocalRef(v);
-        }
-        JAVA->env->ReleaseStringUTFChars(k, kstr);
-        JAVA->env->DeleteLocalRef(k);
-        JAVA->env->DeleteLocalRef(item);
-        JAVA->env->DeleteLocalRef(jterator);
-    }
-    JAVA->env->DeleteLocalRef(res_headers);
-    JAVA->env->DeleteLocalRef(keyset);
-    JAVA->env->DeleteLocalRef(iterator);
-
-    if (conf->need_session == 1) {
-        jobject res_session = JAVA->env->GetObjectField(response_instance, JAVA->res_session);
-        keyset = JAVA->env->CallObjectMethod(res_session, JAVA->hashmap_keyset);
-        iterator = JAVA->env->CallObjectMethod(keyset, JAVA->set_iterator);
-        while ((bool)JAVA->env->CallBooleanMethod(iterator, JAVA->hasnext)) {
-            jstring k = (jstring) JAVA->env->CallObjectMethod(iterator, JAVA->next);
-            jstring v = (jstring) JAVA->env->CallObjectMethod(res_session, JAVA->hashmap_get, k);
-            const char * kstr = JAVA->env->GetStringUTFChars(k, NULL),
-                    * vstr = JAVA->env->GetStringUTFChars(v, NULL);
-            res.session[kstr] = vstr;
-            JAVA->env->ReleaseStringUTFChars(k, kstr);
-            JAVA->env->DeleteLocalRef(k);
-            JAVA->env->ReleaseStringUTFChars(v, vstr);
-            JAVA->env->DeleteLocalRef(v);
-        }
-        JAVA->env->DeleteLocalRef(res_session);
-        JAVA->env->DeleteLocalRef(keyset);
-        JAVA->env->DeleteLocalRef(iterator);
-    }
-
-    res.status = JAVA->env->GetIntField(response_instance, JAVA->status);
-    jstring content = (jstring) JAVA->env->GetObjectField(response_instance, JAVA->content);
-    const char* contentstr = JAVA->env->GetStringUTFChars(content, NULL);
-    res.content = contentstr;
-    JAVA->env->ReleaseStringUTFChars(content, contentstr);
-    JAVA->env->DeleteLocalRef(content);
-}
-
-static bool java_init_handler(ngx_http_hi_loc_conf_t * conf) {
-    if (JAVA_IS_READY)return JAVA_IS_READY;
-    if (!JAVA) {
-        JAVA = std::make_shared<hi::java>((char*) conf->java_classpath.data, (char*) conf->java_options.data, conf->java_version);
-        if (JAVA->is_ok()) {
-            JAVA->request = JAVA->env->FindClass("hi/request");
-            if (JAVA->request != NULL) {
-                JAVA->request_ctor = JAVA->env->GetMethodID(JAVA->request, "<init>", "()V");
-                JAVA->client = JAVA->env->GetFieldID(JAVA->request, "client", "Ljava/lang/String;");
-                JAVA->user_agent = JAVA->env->GetFieldID(JAVA->request, "user_agent", "Ljava/lang/String;");
-                JAVA->method = JAVA->env->GetFieldID(JAVA->request, "method", "Ljava/lang/String;");
-                JAVA->uri = JAVA->env->GetFieldID(JAVA->request, "uri", "Ljava/lang/String;");
-                JAVA->param = JAVA->env->GetFieldID(JAVA->request, "param", "Ljava/lang/String;");
-                JAVA->req_headers = JAVA->env->GetFieldID(JAVA->request, "headers", "Ljava/util/HashMap;");
-                JAVA->form = JAVA->env->GetFieldID(JAVA->request, "form", "Ljava/util/HashMap;");
-                JAVA->cookies = JAVA->env->GetFieldID(JAVA->request, "cookies", "Ljava/util/HashMap;");
-                JAVA->req_session = JAVA->env->GetFieldID(JAVA->request, "session", "Ljava/util/HashMap;");
-
-
-                JAVA->response = JAVA->env->FindClass("hi/response");
-                if (JAVA->response != NULL) {
-                    JAVA->response_ctor = JAVA->env->GetMethodID(JAVA->response, "<init>", "()V");
-                    JAVA->status = JAVA->env->GetFieldID(JAVA->response, "status", "I");
-                    JAVA->content = JAVA->env->GetFieldID(JAVA->response, "content", "Ljava/lang/String;");
-                    JAVA->res_headers = JAVA->env->GetFieldID(JAVA->response, "headers", "Ljava/util/HashMap;");
-                    JAVA->res_session = JAVA->env->GetFieldID(JAVA->response, "session", "Ljava/util/HashMap;");
-
-                    JAVA->hashmap = JAVA->env->FindClass("java/util/HashMap");
-                    JAVA->hashmap_put = JAVA->env->GetMethodID(JAVA->hashmap, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
-                    JAVA->hashmap_get = JAVA->env->GetMethodID(JAVA->hashmap, "get", "(Ljava/lang/Object;)Ljava/lang/Object;");
-                    JAVA->hashmap_keyset = JAVA->env->GetMethodID(JAVA->hashmap, "keySet", "()Ljava/util/Set;");
-
-                    JAVA->arraylist = JAVA->env->FindClass("java/util/ArrayList");
-                    JAVA->arraylist_get = JAVA->env->GetMethodID(JAVA->arraylist, "get", "(I)Ljava/lang/Object;");
-                    JAVA->arraylist_size = JAVA->env->GetMethodID(JAVA->arraylist, "size", "()I");
-                    JAVA->arraylist_iterator = JAVA->env->GetMethodID(JAVA->arraylist, "iterator", "()Ljava/util/Iterator;");
-
-                    JAVA->iterator = JAVA->env->FindClass("java/util/Iterator");
-                    JAVA->hasnext = JAVA->env->GetMethodID(JAVA->iterator, "hasNext", "()Z");
-                    JAVA->next = JAVA->env->GetMethodID(JAVA->iterator, "next", "()Ljava/lang/Object;");
-
-                    JAVA->set = JAVA->env->FindClass("java/util/Set");
-                    JAVA->set_iterator = JAVA->env->GetMethodID(JAVA->set, "iterator", "()Ljava/util/Iterator;");
-                    JAVA_IS_READY = true;
-                }
-            }
-        }
-    }
-    return JAVA_IS_READY;
 }
 
 static std::string md5(const std::string& str) {
